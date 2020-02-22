7b6e38e18354aff5d638dea263c51c76df75d1b9
Artem.Izikaev <aizikaev@gmail.com>
2020-02-22 20:24:39 +0300

    test 2

---
+++ prepare-commit-message.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+try:
+    # get commit message file name
+    commit_message_file_name = os.environ['COMMIT_MSG_FILE']
+
+    # open commit message file and read commit message
+    commit_message_file = open(commit_message_file_name, 'r')
+    data = commit_message_file.read()
+    commit_message_file.close()
+
+    # add branch name before commit message
+    data = op.getenv('BRANCH_NAME') + ". " + data
+
+    # overwrite commit message file
+    commit_message_file = open(commit_message_file_name, "w")
+    commit_message_file.write(data)
+    commit_message_file.close()
+except KeyError:
+    sys.exit(1)
