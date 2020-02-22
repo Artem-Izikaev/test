7f281c928c4a35f50146ec80116e3d69f27b7fe4
Artem.Izikaev <aizikaev@gmail.com>
2020-02-22 21:51:26 +0300

    test. Test 4

--- prepare-commit-message.py
+++ prepare-commit-message.py
@@ -1,12 +1,9 @@
-print(1)
-
 import os
-print(2)
 import sys
-print(3)
+
 try:
     # get commit message file name
-    commit_message_file_name = os.environ['COMMIT_MSG_FILE']
+    commit_message_file_name = os.environ['COMMIT_MSG_FILE1']
 
     # open commit message file and read commit message
     commit_message_file = open(commit_message_file_name, 'r')
@@ -14,12 +11,12 @@ try:
     commit_message_file.close()
 
     # add branch name before commit message
-    data = op.getenv('BRANCH_NAME') + ". " + data
+    data = os.getenv('BRANCH_NAME') + ". " + data
 
     # overwrite commit message file
     commit_message_file = open(commit_message_file_name, "w")
     commit_message_file.write(data)
     commit_message_file.close()
-except KeyError:
-    print("Error!!!")
+except Exception as err:
+    print("Prepare commit message script (.git/hooks/prepare-commit-msg) error: " + str(err))
     sys.exit(1)
